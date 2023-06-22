40e5d332929d5aebeb047e2a9fc74ea4a1f0e16e
Gabor Vilics <vilics@hotmail.com>
2023-06-22 11:50:59 +0200

    [ItemGeneration]: Items are being generated with halls

---
+++ Assets/_/Features/GABORFeatures/PickUpItems/GenerateItemIfEmpty.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateItemIfEmpty : MonoBehaviour
+{
+    public Transform m_carriableObject;
+    public Transform m_itemContainer;
+
+    public void GenerateIfItemTaken()
+    {
+        if (m_itemContainer.childCount == 0)
+        {
+            Instantiate(m_carriableObject, m_itemContainer.position, Quaternion.identity, m_itemContainer);
+        }
+    }
+}

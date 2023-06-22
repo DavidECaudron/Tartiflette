3fe6e6b4da56db0e0e83386b7e536793547ac811
Gabor Vilics <vilics@hotmail.com>
2023-06-22 15:31:04 +0200

    [Portals]: this is fine

---
+++ Assets/_/Features/GABORFeatures/Portals/TestPortal/TpToDestination.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TpToDestination : MonoBehaviour
+{
+    public bool m_ObjectA;
+    public bool m_ObjectB;
+    public bool m_ObjectC;
+}

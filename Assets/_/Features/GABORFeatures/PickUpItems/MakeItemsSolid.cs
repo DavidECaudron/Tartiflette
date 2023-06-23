02ab69148cf96007907576d4192eddfd112c9047
Célor Moon <celor.chapelle.a@gmail.com>
2023-06-23 12:55:08 +0200

    [Final Lvl]

--- Assets/_/Features/GABORFeatures/PickUpItems/MakeItemsSolid.cs
+++ Assets/_/Features/GABORFeatures/PickUpItems/MakeItemsSolid.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 
 public class MakeItemsSolid : MonoBehaviour
 {
+    public GameObject m_passerelle;
+    public bool m_doItYourself;
+    public int m_count;
+
   private void OnCollisionEnter(Collision other)
   {
     if (other.gameObject.CompareTag("Player")) return;
-    other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        switch (m_doItYourself)
+        {
+            case true: other.rigidbody.isKinematic = true; break;
+            case false: m_count++;  SpawnPasserelle(); break;
+        }
   }
+    private void SpawnPasserelle()
+    {
+        if (m_count != 2) return;
+        m_passerelle.SetActive(true);
+    }
 }
